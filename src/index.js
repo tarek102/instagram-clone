ee83abbe2b63a4726a301849fe4575b8e8fbf530
unknown <tarekyosry8@gmail.com>
2023-11-13 21:53:06 +0200

    Connect firebase and seed data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import FirebaseContext from './context/firebase';
+import { FieldValue, firebase } from './lib/firebase';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>
+
+);
 
 
